8124b065460f737fc9c472700baac2b45879ce84
RyanJoy7 <157757179+RyanJoy7@users.noreply.github.com>
2025-03-13 15:54:12 +0000

    Fixed Bugs

--- recipeFinder.py
+++ recipeFinder.py
@@ -6,7 +6,6 @@ import numpy as np
 import re
 
 file = "data/recipeDataset.csv"
-#df = pd.read_csv(file)  //Doesn't work
 df = pd.read_csv(file, encoding='ISO-8859-1')
 
 df.head() # This outputs the Recipe Dataset Table.
@@ -26,7 +25,7 @@ def inputIngredients():
         # Otherwise add the inputed ingredient into the array.
         ingredientsArray.append(ingredient)
     
-    print("ingredients: ", ingredientsArray) # Just for testing.
+    # print("ingredients: ", ingredientsArray) # Just for testing.
 
 
 def suggest_recipes(df, available_ingredients):
@@ -48,14 +47,13 @@ def suggest_recipes(df, available_ingredients):
     # Exclude recipes with zero matches unless available_ingredients is empty.
     if available_ingredients:
         df = df[df["Matches"] > 0] # Exclude recipes with zero matches.
-    top_recipes = df.sort_values(by="Matches", ascending=False).head(3)
-    return top_recipes[["Name", "Method", "Ingredients", "Matches"]]
+    top_recipes = df.sort_values(by="Matches", ascending=False).head(10)
+    return top_recipes[["Name", "Ingredients", "Matches"]]
 
 # Example usage
 #ingredientsArray = ['baking', 'potatoes', 'pepper', 'coriander', 'salt', 'onion', 'garlic', 'tomato', 'chicken', 'oil', 'water']
 inputIngredients() # Calls the method which allows the user to input the ingredients.
 top = suggest_recipes(df, available_ingredients=ingredientsArray)
+
 if top is not None:
     print(top)
-
-
